ed270933cd7af84325e5094b99a2614cd927a399
Jeffrey <Lenovo@-JEFFREY>
2016-12-06 14:25:36 -0600

    Solucion conectada, el metodo esta en una clase conexion solo la jala de

---
+++ ASysMec/ConexionBD/Conexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace ASysMec.ConexionBD
+{
+    public class Conexion
+    {
+        public SqlConnection conexion;
+
+        public Conexion()
+        {
+            
+        }
+
+        public void CrearConexion()
+        {
+            try
+            {
+                string cadena = "Data Source = -JEFFREY\\SQLEXPRESSJEFRY; Initial Catalog = BD_SysMec; Integrated Security = True";
+                conexion = new SqlConnection(cadena);
+                conexion.Open();
+            }
+            catch (Exception) { }
+        }
+
+        public SqlConnection getConexion()
+        {
+            try
+            {
+                return conexion;
+            }
+            catch (Exception) { return null; }
+        }
+    }
+}
