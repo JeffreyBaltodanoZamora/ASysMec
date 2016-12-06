01ec3cf0852d71e905ac7a366a0c72980db19a8b
Lenovo <Lenovo@-JEFFREY>
2016-12-06 14:02:35 -0600

    Add project files.

---
+++ ASysMec/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASysMec.Startup))]
+namespace ASysMec
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
